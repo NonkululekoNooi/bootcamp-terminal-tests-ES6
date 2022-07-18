b652b84e234b77066f822f8fe20009ecb8899e25
NonkululekoNooi <nooi9423@gmail.com>
2022-07-18 10:24:54 +0200

    added

--- test/mostProfitableDepartment.tests.js
+++ test/mostProfitableDepartment.tests.js
@@ -66,6 +66,27 @@ describe('determine which of departments are the most profitable', function () {
         ];
         assert.equal(mostProfitableDepartment(salesData3), 'hardware')
     })
+
+    it("It should return 'outdoor' when it is called by the 'mostProfitableDepartment' salesData ", function () {
+        var salesData4 = [
+          
+            { department: 'carpentry', sales: 5500, day: 'Monday' },
+            { department: 'steelwork', sales: 7500, day: 'Tuesday' },
+            { department: 'carpentry', sales: 1540, day: 'Tuesday' },
+            { department: 'hardware', sales: 1500, day: 'Wednesday' },
+            { department: 'outdoor', sales: 8507, day: 'Wednesday' },
+            { department: 'carpentry', sales: 8009, day: 'Wednesday' },
+            { department: 'hardware', sales: 12000, day: 'Thursday' },
+            { department: 'carpentry', sales: 6109, day: 'Thursday' },
+            { department: 'hardware', sales: 7005, day: 'Friday' },
+            { department: 'steelwork', sales: 7500, day: 'Tuesday' },
+            { department: 'outdoor', sales: 2505, day: 'Tuesday' },
+            { department: 'carpentry', sales: 1540, day: 'Tuesday' },
+            { department: 'steelwork', sales: 1500, day: 'Wednesday' },
+            { department: 'carpentry', sales: 8009, day: 'Wednesday' },
+        ];
+        assert.equal(mostProfitableDepartment(salesData4), 'carpentry')
+    })
    
 })
 
