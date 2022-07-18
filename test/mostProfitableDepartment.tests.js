afbe6555c50a1c7b0245b65d737f9473af163e6f
NonkululekoNooi <nooi9423@gmail.com>
2022-07-18 09:59:15 +0200

    added

--- test/mostProfitableDepartment.tests.js
+++ test/mostProfitableDepartment.tests.js
@@ -47,5 +47,25 @@ describe('determine which of departments are the most profitable', function () {
         ];
         assert.equal(mostProfitableDepartment(salesData2), 'electronics')
     })
-
+    
+    it("It should return 'hardware' when it is called by the 'mostProfitableDepartment' salesData ", function () {
+        var salesData3 = [
+            { department: 'hardware', sales: 4500, day: 'Monday' },
+            { department: 'outdoor', sales: 1500, day: 'Monday' },
+            { department: 'carpentry', sales: 5500, day: 'Monday' },
+            { department: 'hardware', sales: 7500, day: 'Tuesday' },
+            { department: 'carpentry', sales: 1540, day: 'Tuesday' },
+            { department: 'hardware', sales: 1500, day: 'Wednesday' },
+            { department: 'outdoor', sales: 8507, day: 'Wednesday' },
+            { department: 'carpentry', sales: 8009, day: 'Wednesday' },
+            { department: 'hardware', sales: 12000, day: 'Thursday' },
+            { department: 'carpentry', sales: 6109, day: 'Thursday' },
+            { department: 'hardware', sales: 7005, day: 'Friday' },
+           
+          
+        ];
+        assert.equal(mostProfitableDepartment(salesData3), 'hardware')
+    })
+   
 })
+
